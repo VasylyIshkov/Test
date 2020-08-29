426273cbbc3dd6fff76b62b8e53a53205d887884
Xamarin <formyl17@gmail.com>
2020-08-29 19:02:30 +0300

    Закончил интерфейс и логику

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MotionEvent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.test.fragment.MainFragment;
 
+public class MainActivity extends AppCompatActivity  implements View.OnTouchListener{
+    private MainFragment mainFragment;
+
+    private FrameLayout conteiner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        conteiner = findViewById(R.id.fragment_container);
+        mainFragment = new MainFragment();
+        getSupportFragmentManager().beginTransaction().add(conteiner.getId(), mainFragment).commit();
+
+    }
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int color = v.getDrawingCache(true).getPixel((int) event.getX(), (int) event.getY());
+//if (color!="#301E10")
+
+        Log.println(Log.DEBUG, "??????", color + "");
+        return true;
     }
 }
--- app/src/main/java/com/example/test/fragment/MainFragment.java
+++ app/src/main/java/com/example/test/fragment/MainFragment.java
@@ -1,21 +1,36 @@
 package com.example.test.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.test.R;
 
-public class MainFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainFragment extends Fragment implements View.OnTouchListener {
+
+    private ImageButton menu, pause, normal, accelerated, shop;
+    private TextView date;
+    View view;
 
     public MainFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -28,6 +43,103 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        initFragment();
+
+        return view;
+    }
+
+    private void initFragment() {
+        menu = view.findViewById(R.id.bnt_menu);
+        pause = view.findViewById(R.id.bnt_pause);
+        normal = view.findViewById(R.id.bnt_normal);
+        accelerated = view.findViewById(R.id.bnt_accelerated);
+        shop = view.findViewById(R.id.bnt_shop);
+        date = view.findViewById(R.id.date);
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        date.setText(format.format(new Date()));
+        setListeners();
+    }
+
+    private void buttonLogic(int id) {
+        final int bntNumber = id;
+        DialogInterface.OnClickListener myClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (bntNumber) {
+                    case 1:
+                        menu.setImageResource(R.mipmap.tb_ic_menu_foreground);
+                        break;
+                    case 2:
+                        shop.setImageResource(R.mipmap.tb_ic_shop_foreground);
+                        break;
+                    case 3:
+                        pause.setImageResource(R.mipmap.tb_ic_pause_foreground);
+                        break;
+                    case 4:
+                        normal.setImageResource(R.mipmap.tb_ic_normal_foreground);
+                        break;
+                    case 5:
+                        accelerated.setImageResource(R.mipmap.tb_ic_accelerated_foreground);
+                        break;
+                }
+
+            }
+        };
+        AlertDialog.Builder dialog = new AlertDialog.Builder(view.getContext());
+        dialog.setMessage(id + "");
+        dialog.setPositiveButton("Ok", myClickListener);
+        dialog.create();
+        dialog.show();
+
+
+    }
+
+    private void setListeners() {
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setImageResource(R.mipmap.tb_ic_menu_clicked_foreground);
+                buttonLogic(1);
+            }
+        });
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shop.setImageResource(R.mipmap.tb_ic_shop_clicked_foreground);
+                buttonLogic(2);
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause.setImageResource(R.mipmap.tb_ic_pause_clicked_foreground);
+                buttonLogic(3);
+            }
+        });
+        normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.println(Log.DEBUG, "??????", "|");
+                normal.setImageResource(R.mipmap.tb_ic_normal_clicked_foreground);
+                buttonLogic(4);
+            }
+        });
+        accelerated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.DEBUG, "??????", "|");
+                accelerated.setImageResource(R.mipmap.tb_ic_accelerated_clicked_foreground);
+                buttonLogic(5);
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int color = v.getDrawingCache(true).getPixel((int) event.getX(), (int) event.getY());
+//if (color!="#301E10")
+        Log.println(Log.DEBUG, "??????", color + "");
+        return true;
     }
 }
